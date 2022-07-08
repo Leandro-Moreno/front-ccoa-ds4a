5a84cec3f2cd8125d402ee13182717b394e5a585
Leandro Moreno <123857@gmail.com>
2022-07-07 23:48:48 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from "vue"
-import Navbar from "../components/NavBar.vue"
-import Footer from "../components/Footer.vue"
+import Navbar from "./components/Navbar.vue"
+import Footer from "./components/Footer.vue"
 
 </script>
 
