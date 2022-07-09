91ebe9275fb2d31754ea61aecfd81dd049047da5
Leandro Moreno <123857@gmail.com>
2022-07-09 18:03:50 -0500

    changes in texts

--- src/routes.js
+++ src/routes.js
@@ -2,40 +2,49 @@ import Home from './views/Home.vue'
 import About from './views/About.vue'
 import NotFound from './views/NotFound.vue'
 import Registro from './views/Registro.vue';
-import Resultado  from "./views/Resultado.vue";
-import EDA  from "./views/EDA.vue";
+import Resultado from "./views/Resultado.vue";
+import EDA from "./views/EDA.vue";
 
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
-  { path: '/', component: Home, meta: { title: 'Inicio' } },
+    {
+      path: '/',
+      component: Home,
+      meta: {title: 'Inicio'}
+    },
   {
-    path: '/objetivo',
-    meta: { title: 'Acerca del proyecto' },
-    component: About,
-    // example of route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import('./views/About.vue')
-  },
-  {
-    path: '/somos',
-    meta: { title: 'Quienes Somos' },
-    component: Home,
-  },
-  {
-    path: '/eda',
-    meta: { title: 'EDA' },
-    component: EDA,
-  },
-  {
-    path: '/formulario',
-    meta: { title: 'Formulario' },
-    component: Registro,
-  },
-  {
-    path: '/resultados/:result',
-    meta: { title: 'Resultados' },
-    component: Resultado,
-  },
-  { path: '/:path(.*)', component: NotFound },
+      path: '/inicio',
+      component: Home,
+      meta: {title: 'Inicio'}
+    },
+    {
+        path: '/objetivo',
+        meta: {title: 'Acerca del proyecto'},
+        component: About,
+        // example of route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        // component: () => import('./views/About.vue')
+    },
+    {
+        path: '/somos',
+        meta: {title: 'Quienes Somos'},
+        component: Home,
+    },
+    {
+        path: '/eda',
+        meta: {title: 'EDA'},
+        component: EDA,
+    },
+    {
+        path: '/formulario',
+        meta: {title: 'Formulario'},
+        component: Registro,
+    },
+    {
+        path: '/resultados/:result',
+        meta: {title: 'Resultados'},
+        component: Resultado,
+    },
+    {path: '/:path(.*)', component: NotFound},
 ]
--- src/views/About.vue
+++ src/views/About.vue
@@ -4,8 +4,27 @@
 <template>
   <div class="bg-gray-50">
     <div
-      class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8"
+        class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:py-16 lg:px-8"
     >
+      <h1 class="text-6xl font-normal leading-normal mt-0 mb-2 text-pink-800">
+        Objetivo del proyecto
+      </h1>
+      <p class="text-base font-light leading-relaxed mt-0 mb-4 text-pink-800">
+        La CÁMARA DE COMERCIO DEL ORIENTE ANTIOQUEÑO, abreviada como CCOA, es una organización privada sin fines de lucro que realiza funciones delegadas por el Gobierno Nacional y brinda servicios encaminados al fortalecimiento comercial y empresarial en la región de su jurisdicción.
+        El objetivo principal de la CCOA es ayudar a los comerciantes y empresarios de la región mediante la formalización de la actividad económica, el fortalecimiento y desarrollo de negocios y el aumento de la competitividad regional.
+        La CCOA tiene jurisdicción sobre el Oriente Antioqueño, el cual está integrado por 23 municipios divididos en cuatro zonas con base en dinámicas socioeconómicas, culturales y físico-naturales homogéneas, las cuales son las siguientes:
+        Altiplano: Rionegro, La Ceja, Guarne, Marinilla, La Unión, San Vicente, El Carmen de Viboral, El Santuario y El Retiro
+        Embalses: El Peñol, Guatapé, San Carlos, San Rafael, Alejandría, Granada y Concepción
+        Páramo: Sonsón, Argelia, Abejorral y Nariño
+        Bosques: San Luis, San Francisco y Cocorná
+        Actualmente, la CCOA cuenta con una base de datos de las distintas empresas de su jurisdicción (esta información la proporciona cada empresa al momento de registrarse o actualizar su registro mercantil), pero carecen de modelos que permitan analizar el comportamiento de las diferentes empresas a lo largo del tiempo, como el análisis por sectores económicos, actividad empresarial, sectores de mayor crecimiento, probabilidad de creación/mortalidad de empresas, etc.
+        Lo anterior permitirá una amplia observación del comportamiento de las diversas empresas y sectores del oriente antioqueño para informar, tomar decisiones y promover y/o proponer actividades que permitan a las distintas empresas mitigar los riesgos de no sobrevivir, así como proponer políticas públicas con el objetivo de fortalecer las empresas, tener un mayor impacto en la competitividad y promover el espíritu emprendedor.
+        El siguiente proyecto tiene como objetivo solucionar lo siguiente:
+        ¿Cuál es el ciclo de vida de una empresa en el Oriente Antioqueño a partir del dataset de los últimos 10 años del CCOA?
+        En base a los datos, de organizaciones anteriores, cuál es el capital inicial que debe tener cada sector para emprender
+        Realizar un análisis de los diferentes sectores económicos que conforman la Cámara de Comercio del Oriente Antioqueño.
+        Lo anterior permitirá observar ampliamente el comportamiento de las diferentes empresas y sectores del oriente antioqueño, con el fin de informar, tomar decisiones y promover y/o proponer actividades que permitan a las diferentes empresas mitigar los riesgos de no sobrevivir y así mismo proponer políticas públicas con el objetivo de fortalecer a las empresas, tener un mayor impacto en la competitividad y fomentar el espíritu emprendedor.
+      </p>
     </div>
   </div>
 </template>
