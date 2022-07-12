2c3c7ab1f654fdd5bc62a2848c8ea200fafed683
Leandro Moreno <123857@gmail.com>
2022-07-11 19:28:53 -0500

    Update Registro.vue

--- src/views/Registro.vue
+++ src/views/Registro.vue
@@ -217,7 +217,7 @@ export default {
       //and set the value to the sector
       let sector = this.sectores[this.LCIIU]
 
-      await axios.get('http://35.202.169.29/model', config,
+      await axios.get('https://back.leandromoreno.com/model', config,
           {
             headers: {
               "Access-Control-Allow-Origin": "*"
